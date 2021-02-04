bcbbd51a62b3625f8c30b8940b36b42bf75d3809
Daniel Kim <chipperday@gmail.com>
2021-02-04 14:38:43 -0800

    solved (AC)

--- usaco/2020/1/greedy.py
+++ usaco/2020/1/greedy.py
@@ -1,1 +1,28 @@
+def solve():
+  n = int(input())
+  students = input()
 
+  i = 0
+  while(i < len(students) and students[i] != 'A'):
+    i += 1
+  
+  count = 0
+  maxCount = 0
+  
+  for j in range(i, len(students)):
+    if students[j] == 'P':
+      count += 1
+    else:
+      count = 0
+
+    maxCount = max(maxCount, count)
+
+  print(maxCount)
+
+
+
+
+
+t = int(input())
+for i in range(t):
+  solve()
